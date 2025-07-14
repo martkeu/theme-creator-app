5b97ddc989436cb7f44f20fe21df9fdd2bcdf18d
Martin <martkeu@web.de>
2025-07-14 14:39:11 +0200

    fetch color names according to hex-values

--- src/components/ColorCardDetails/ColorCardDetails.jsx
+++ src/components/ColorCardDetails/ColorCardDetails.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import './ColorCardDetails.css';
 
 /*-----------------------------------------------------------------------------mk--
@@ -7,12 +8,36 @@ import './ColorCardDetails.css';
 | - name
 | - role
 | - hex value
+|
+| Fetching Color-Names according to hex-values
+| - fetching as a site effect
+| - handle as a state-variable (colorName) 
+| - rendering as a view component
 */
 function ColorCardDetails({ color }) {
+    const [colorName, setColorName] = useState('');
+
+    const colorSineHashSign = color.value.slice(1);
+    
+    const url = `https://www.thecolorapi.com/id?hex=${colorSineHashSign}`;
+
+	useEffect(() => {
+		async function fetchData() {
+			const resp = await fetch(url);
+            const data = await resp.json();
+            
+            console.log(data.name.value);
+
+            setColorName(data.name.value);
+        }
+        
+        fetchData()
+	}, [url]);
+
 	return (
 		<article className="color-card">
 			<div className="color-card__info">
-				<p className="color-card__name">{color.name}</p>
+				<p className="color-card__name">{colorName}</p>
 				<p className="color-card__role">{color.role}</p>
 				<p className="color-card__hex">{color.value}</p>
 			</div>
