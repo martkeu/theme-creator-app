03aa097ffacaa622e9b3a15e098b840730fda460
Martin <martkeu@web.de>
2025-07-15 08:10:40 +0200

    improve ThemeEditForm

--- src/components/ThemeEditForm/ThemeEditForm.jsx
+++ src/components/ThemeEditForm/ThemeEditForm.jsx
@@ -2,17 +2,17 @@ import './ThemeEditForm.css';
 
 export default function ThemeEditForm({ theme, onEditTheme }) {
 	function handleSubmit(event) {
-        event.preventDefault();
-        
-		const formData = new FormData(event.target);
-        const data = Object.fromEntries(formData);
-        
-        console.log(data, theme.id);
+		event.preventDefault();
 
-        onEditTheme({...data, id: theme.id});
-        
-        event.target.reset();
-        event.target.elements.title.focus();
+		const formData = new FormData(event.target);
+		const data = Object.fromEntries(formData);
+
+		console.log(data, theme.id);
+
+		onEditTheme({ ...data, id: theme.id });
+
+		event.target.reset();
+		event.target.elements.title.focus();
 	}
 
 	return (
@@ -22,33 +22,69 @@ export default function ThemeEditForm({ theme, onEditTheme }) {
 			<div className="theme-form__fields">
 				<div className="theme-form__field">
 					<label htmlFor="title">Theme-Name</label>
-                    <input type="text" name="title" id="title" defaultValue={theme.name} required/>
-                </div>
-                
-                <div className="theme-form__field">
-                    <label htmlFor="colors">Theme-Colors</label>
+					<input
+						type="text"
+						name="title"
+						id="title"
+						defaultValue={theme.name}
+						required
+					/>
+				</div>
 
-                    <div>
-					    <label htmlFor="color1" className="screen-reader-only">Color 1</label>
-                        <input type="color" name="color1" id="color1" defaultValue={theme.colors[0].value} />
-                    </div>
-                    <div>
-					    <label htmlFor="color2" className="screen-reader-only">Color 1</label>
-                        <input type="color" name="color2" id="color2" defaultValue={theme.colors[1].value} />
-                    </div>
-                    <div>
-					    <label htmlFor="color3" className="screen-reader-only">Color 1</label>
-                        <input type="color" name="color3" id="color3" defaultValue={theme.colors[2].value} />
-                    </div>
-                    <div>
-					    <label htmlFor="color4" className="screen-reader-only">Color 1</label>
-                        <input type="color" name="color4" id="color4" defaultValue={theme.colors[3].value} />
-                    </div>
-                </div>
-                
+				<div className="theme-form__field">
+					<label htmlFor="colors">Theme-Colors</label>
+
+					<div className="theme-form__colors">
+						<div>
+							<label htmlFor="color1" className="screen-reader-only">
+								Color 1
+							</label>
+							<input
+								type="color"
+								name="color1"
+								id="color1"
+								defaultValue={theme.colors[0].value}
+							/>
+						</div>
+						<div>
+							<label htmlFor="color2" className="screen-reader-only">
+								Color 1
+							</label>
+							<input
+								type="color"
+								name="color2"
+								id="color2"
+								defaultValue={theme.colors[1].value}
+							/>
+						</div>
+						<div>
+							<label htmlFor="color3" className="screen-reader-only">
+								Color 1
+							</label>
+							<input
+								type="color"
+								name="color3"
+								id="color3"
+								defaultValue={theme.colors[2].value}
+							/>
+						</div>
+						<div>
+							<label htmlFor="color4" className="screen-reader-only">
+								Color 1
+							</label>
+							<input
+								type="color"
+								name="color4"
+								id="color4"
+								defaultValue={theme.colors[3].value}
+							/>
+						</div>
+					</div>
+				</div>
+
 				<div className="theme-form__button-wrapper">
-                    {/* <Button type="submit">Create</Button> */}
-                    <button type="submit">Update</button>
+					{/* <Button type="submit">Create</Button> */}
+					<button type="submit">Update Theme</button>
 				</div>
 			</div>
 		</form>
